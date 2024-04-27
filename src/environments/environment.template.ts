69768a1a02190958d5633f35ae407810de361e11
Dante Arrighi <dantearrighi@gmail.com>
2024-04-27 14:07:47 -0300

    Added Heroes.service. Prepare environments to store Marvel API Keys

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,8 @@
+
+// Replace the following with your own Marvel API keys
+export const environment = {
+  production: false,
+  marvelPublicKey: 'REPLACE_WITH_YOUR_PUBLIC_KEY',
+  marvelPrivateKey: 'REPLACE_WITH_YOUR_PRIVATE_KEY',
+};
+
