63e99e2e57ef5ae2e682c3e31c92b5025ca96e97
Dante Arrighi <54290020+arrighidante@users.noreply.github.com>
2024-04-30 11:39:42 -0300

    Update environment.ts with a comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,6 +12,7 @@ export const environment = {
 
   */
 
+  // This keys will be stored here just for the purpose of this challenge. I'll delete them after the review.
   marvelPrivateKey: '177f53bba1305b5f8b8a42d6e8755dff2d611c78',
   marvelPublicKey: 'ade7bf1ea984a87bd9042f9599a1af16',
   apiMarvelURL: 'https://gateway.marvel.com:443/v1/public',
